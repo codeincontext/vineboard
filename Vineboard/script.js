86b4bf363dca23a2e17c09a5cc4391d84e46224b
Adam Howard <adz@skatty.me>
2013-04-20 19:02:02 +0100

    Add overdrive

--- Vineboard/script.js
+++ Vineboard/script.js
@@ -29,8 +29,9 @@ var keys = {};
 
 var sustain = false;
 var sustainedNotes = [];
-var audioContext, preEffectNode, convNode;
+var audioContext, preEffectNode, convNode, waveShaper;
 var convolution = false;
+var overdrive = false;
 
 function enableConvolution() {
   preEffectNode.disconnect();
@@ -42,26 +43,51 @@ function disableConvolution() {
   preEffectNode.connect(audioContext.destination);
 }
 
+function enableOverdrive() {
+  preEffectNode.disconnect();
+  preEffectNode.connect(waveShaper);
+}
+
+function disableOverdrive() {
+  preEffectNode.disconnect();
+  preEffectNode.connect(audioContext.destination);
+}
+
 function setupAudio() {
+  // Base audio
   audioContext = new webkitAudioContext;
   preEffectNode = audioContext.createGainNode();
   preEffectNode.connect(audioContext.destination);
 
+  // Convolution
   convNode = audioContext.createConvolver();
   var convGainNode = audioContext.createGainNode();
-  // conv is quiet. boost it a bit
-  var gainNode = audioContext.createGainNode();
   convNode.connect(convGainNode);
-  convGainNode.connect(gainNode);
-  gainNode.connect(audioContext.destination);
-  gainNode.gain.value = 4.0;
+  convGainNode.connect(audioContext.destination);
+  convGainNode.gain.value = 4.0;  // conv is quiet. boost it a bit
+
+  // Overdrive
+  var lowPassFreq = 7000;
+  var lowPass = audioContext.createBiquadFilter();
+  lowPass.type = 0;
+  lowPass.frequency.value = lowPassFreq;
+  waveShaper = audioContext.createWaveShaper();
+  waveShaper.connect(lowPass);
+  lowPass.connect(audioContext.destination);
+    var k = 40; // this number matters
+    var n_samples = 22050;
+    var wsCurve = new Float32Array(n_samples);
+    var deg = Math.PI / 180;
+    for (var i = 0; i < n_samples; i += 1) {
+        var x = i * 2 / n_samples - 1;
+        wsCurve[i] = (3 + k) * x * 20 * deg / (Math.PI + k * Math.abs(x));
+    }
+    waveShaper.curve = wsCurve;
 
 
   request = new XMLHttpRequest();
-
   request.open('GET', 'AK-SPKRS_VinUs_002.wav', true);
   request.responseType = 'arraybuffer';
-
 
   request.onload = function() {
       audioContext.decodeAudioData(/** @type {ArrayBuffer} */(request.response), function(buffer) {
@@ -84,18 +110,6 @@ function init() {
 }
 window.onload = init;
 
-// var gainNode = 
-// var videoElement = document.querySelector('video');
-// var mediaSourceNode = context.createMediaElementSource(audioElement);
-// mediaSourceNode.connect(filter);
-// filter.connect(context.destination);
-
-// var mediaElement = document.getElementById('mediaElementID');
-// var sourceNode = context.createMediaElementSource(mediaElement);
-// sourceNode.connect(audioContext.destination);
-
-
-// this.gainNode.connect(audioContext.destination);
 
 var onkeydown = function(aEvent) {
     // console.log(aEvent.keyCode);
@@ -139,11 +153,17 @@ var onkeydown = function(aEvent) {
     } else if (aEvent.keyCode == 188) {
       if (convolution) {
         disableConvolution();
-      }  else {
+      } else {
         enableConvolution();
       }
       convolution = !convolution;
     } else if (aEvent.keyCode == 190) {
+      if (overdrive) {
+        disableOverdrive();
+      } else {
+        enableOverdrive();
+      }
+      overdrive = !overdrive;
     } else if (aEvent.keyCode == 191) {
 
     } else {
